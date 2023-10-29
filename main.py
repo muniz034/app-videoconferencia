c418731853aaab77fa64ff93f67cb0f0143152e6
Guilherme Moreira <gfmoreira@id.uff.br>
2023-10-29 11:03:36 -0300

    Criei a classe ponto e os métodos iniciar e conectar,essa parte eu fui inspirado, o resto será nosso

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-#A atividade prática da disciplina consiste em desenvolver uma aplicação de videoconferência descentralizada. Para isso, deve ser #utilizada comunicação por sockets, permitindo que os usuários primeiro se registrem em um servidor e consultem a lista de nós #cadastrados, para depois de conectarem aos seus pares utilizando o modelo Peer-to-Peer (P2P).
+#A atividade prática da disciplina consiste em desenvolver uma aplicação de videoconferência descentralizada. 
+#Para isso, deve ser utilizada comunicação por sockets, permitindo que os usuários primeiro se registrem em um servidor e 
+#consultem a lista de nós cadastrados, para depois de conectarem aos seus pares utilizando o modelo Peer-to-Peer (P2P).
 
 #O socket cliente deve:
 #Registrar-se no servidor utilizando um nome e um IP exclusivos e indicando a porta apta para receber o pedido de chamada;
@@ -12,7 +14,23 @@
 #Responder aos clientes o nome de um nó conectado e seus respectivos endereços e números de porta, quando assim solicitado;
 #Caso o cliente solicite o fim da conexão, o servidor deve responder com mensagem de encerramento e, depois, fechar o socket.
 
-import socketserver
+import socket
+import threading
 
-s = socketserver
-print(s.TCPServer.__qualname__)
+class Ponto:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conexoes = []
+        
+    def conectar(self, ponto_host, ponto_port):
+        try:
+            conexao = self.socket.connect((ponto_host, ponto_port))
+            self.conexoes.append(conexao)
+            print(f'Conexão bem-sucedida com host: {ponto_host} | porta: {ponto_port}')
+        except socket.error as e:
+            print(f'Conexão mal-sucedida com host: {ponto_host} | porta: {ponto_port}')
+            print(f'Erro encontrado: {e}')
+
+#169.254.150.66/16
